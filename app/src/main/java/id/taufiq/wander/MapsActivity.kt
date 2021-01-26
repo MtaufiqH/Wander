d2cfb57df745ab166e3f690ae062d5323c0d30db
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-01-26 11:04:12 +0800

    adding marker to my home

--- app/src/main/java/id/taufiq/wander/MapsActivity.kt
+++ app/src/main/java/id/taufiq/wander/MapsActivity.kt
@@ -36,10 +36,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = -5.090016
+        val longitude = 119.520791
+        val zoomLevel = 15f
+
+        val homeLatLang = LatLng(latitude,longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLang, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLang).title("my home"))
+
     }
 
 
