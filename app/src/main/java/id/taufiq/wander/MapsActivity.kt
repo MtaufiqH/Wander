9e047033d51ef1c965ed1d58422536774fa417ef
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-01-26 11:35:27 +0800

    change style of the maps and change marker icon

--- app/src/main/java/id/taufiq/wander/MapsActivity.kt
+++ app/src/main/java/id/taufiq/wander/MapsActivity.kt
@@ -1,6 +1,8 @@
 package id.taufiq.wander
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +10,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    private val TAG = MapsActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +48,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLang).title("my home"))
         setMapLongClick(map)
         setPoiClick(map)
+        setMapStyle(map)
 
     }
 
@@ -59,6 +62,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 position(latLang)
                 title(getString(R.string.dropped_pin))
                 snippet(snippet)
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
             })
         }
     }
@@ -71,6 +75,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             })
 
             poiMarker.showInfoWindow()
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap){
+        try {
+            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style))
+            if (!success){
+                Log.e(TAG, "setMapStyle: unsuccessful")
+            }
+
+        } catch (e : Resources.NotFoundException){
+            Log.e(TAG, "can't find style, error")
         }
     }
 
